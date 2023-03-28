9016641b37839a1a0478a0b633d5f3a40d98c80d
tero1115 <limjisang1115@gmail.com>
2023-03-28 12:21:03 +0900

    컴포넌트 설계 및 기본세팅

---
+++ lib/views/pages/post/home/home_body_popular.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeBodyPopular extends StatelessWidget {
+  const HomeBodyPopular({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/views/pages/post/home/home_header_appbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeHeaderAppbar extends StatelessWidget {
+  const HomeHeaderAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
